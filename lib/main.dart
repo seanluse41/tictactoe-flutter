77cc452f072157bc71de0dbe0aa5deadcbcd50d8
sean <seanluse41@gmail.com>
2020-03-01 19:10:05 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/gamescreen.dart';
+
+void main() => runApp(TicTacToe());
+
+class TicTacToe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GameScreen(),
+    );
+  }
+}
---
+++ lib/screens/gamescreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("hey"),
+          ),
+        ],
+      ),
+    );
+  }
+}
