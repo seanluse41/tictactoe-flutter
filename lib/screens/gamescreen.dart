863abde5e2a17c8b5f3057bad2a6891787975082
sean <seanluse41@gmail.com>
2020-03-06 12:56:20 +0900

    add scoreboard.

--- lib/screens/gamescreen.dart
+++ lib/screens/gamescreen.dart
@@ -7,9 +7,11 @@ class GameScreen extends StatefulWidget {
 
 class _GameScreenState extends State<GameScreen> {
   bool ohTurn = false; // x goes first
+  int xScore = 0;
+  int oScore = 0;
 
   List<String> displayExOh = [
-    "", // one space to avoid win condition
+    "",
     "",
     "",
     "",
@@ -24,97 +26,162 @@ class _GameScreenState extends State<GameScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      body: GridView.builder(
-          itemCount: 9,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                _tapped(index);
-              },
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
               child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey[700],
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    displayExOh[index],
-                    style: TextStyle(color: Colors.white, fontSize: 40),
-                  ),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "SCOREBOARD",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "PLAYER X:  $xScore",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "PLAYER O:  $oScore",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            );
-          }),
+            ),
+            Expanded(
+              flex: 4,
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      _tapped(index);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          displayExOh[index],
+                          style: TextStyle(color: Colors.white, fontSize: 40),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   void _tapped(index) {
     setState(() {
-      if (ohTurn) {
+      if (ohTurn && displayExOh[index] != "") {
         displayExOh[index] = "O";
-      } else {
+        ohTurn = !ohTurn;
+      } else if (!ohTurn && displayExOh[index] != "") {
         displayExOh[index] = "X";
+        ohTurn = !ohTurn;
       }
-      ohTurn = !ohTurn;
       _checkWinner();
     });
   }
 
   void _checkWinner() {
-    if (displayExOh.every((square) => square.isNotEmpty)) {
-      // first row
-      if (displayExOh[0] == displayExOh[1] &&
-          displayExOh[0] == displayExOh[2]) {
-        _showWinDialog(displayExOh[0]);
-      }
-      // second row
-      if (displayExOh[3] == displayExOh[4] &&
-          displayExOh[3] == displayExOh[5]) {
-        _showWinDialog(displayExOh[3]);
-      }
-      //third row
-      if (displayExOh[6] == displayExOh[7] &&
-          displayExOh[6] == displayExOh[8]) {
-        _showWinDialog(displayExOh[6]);
-      }
-      //first column
-      if (displayExOh[0] == displayExOh[3] &&
-          displayExOh[0] == displayExOh[6]) {
-        _showWinDialog(displayExOh[0]);
-      }
-      //second column
-      if (displayExOh[1] == displayExOh[4] &&
-          displayExOh[1] == displayExOh[7]) {
-        _showWinDialog(displayExOh[1]);
-      }
-      //third column
-      if (displayExOh[2] == displayExOh[5] &&
-          displayExOh[2] == displayExOh[8]) {
-        _showWinDialog(displayExOh[2]);
-      }
-      //diagnol
-      if (displayExOh[6] == displayExOh[4] &&
-          displayExOh[6] == displayExOh[2]) {
-        _showWinDialog(displayExOh[6]);
-      }
-      //diagnol
-      if (displayExOh[0] == displayExOh[4] &&
-          displayExOh[0] == displayExOh[8]) {
-        _showWinDialog(displayExOh[0]);
-      }
+    // first row
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != "") {
+      _showWinDialog(displayExOh[0]);
+    }
+    // second row
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != "") {
+      _showWinDialog(displayExOh[3]);
+    }
+    //third row
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != "") {
+      _showWinDialog(displayExOh[6]);
+    }
+    //first column
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != "") {
+      _showWinDialog(displayExOh[0]);
+    }
+    //second column
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != "") {
+      _showWinDialog(displayExOh[1]);
+    }
+    //third column
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != "") {
+      _showWinDialog(displayExOh[2]);
+    }
+    //diagnol
+    if (displayExOh[6] == displayExOh[4] &&
+        displayExOh[6] == displayExOh[2] &&
+        displayExOh[6] != "") {
+      _showWinDialog(displayExOh[6]);
+    }
+    //diagnol
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != "") {
+      _showWinDialog(displayExOh[0]);
     }
   }
 
   void _showWinDialog(String winner) {
     showDialog(
+        barrierDismissible: false,
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text("WINNER IS:" + winner),
+            title: Text("WINNER IS: " + winner),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: _clearBoard,
+                child: Text("PLAY AGAIN?"),
+              )
+            ],
           );
         });
+    if (winner == "O") {
+      oScore += 1;
+    } else if (winner == "X") {
+      xScore += 1;
+    }
   }
+
+  void _clearBoard() {}
 }
